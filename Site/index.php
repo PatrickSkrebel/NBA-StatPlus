9640205f18c6c2831623baba37fb0753ea204f39
PatrickSkrebel <peskrebel@email.neit.edu>
2023-12-04 12:06:36 -0500

    starting final project

---
+++ Site/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <style>
+    .left {
+        margin: 200px 0 0 20px; /* top right bottom left */
+    }
+    .right {
+        margin: -107px 0px 0 200px; /* top right bottom left */
+    }
+    </style>
+
+    <title>Stat+</title>
+</head>
+
+<body>
+    <div name="News">
+        <div class="left">
+            <p>First News Sections</p>
+            <image src="http://www.google.com"/>
+        </div></br></br>
+        <div class="right">
+            <p>Second News Sections</p>
+            <image src="http://www.google.com"/>
+        </div></br>
+    </div>
+    
+</body>
+</html>
